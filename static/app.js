80544cee0b4ac1809edf100de33b7c1795e77159
songdd <18611640508@126.com>
2026-08-01 15:35:38 +0800

    bug fixed

--- static/app.js
+++ static/app.js
@@ -202,7 +202,7 @@ async function deleteTask(id) {
     var actionButtons = "";
     var peerInput = "";
     if (t.link_type === "magnet" || t.link_type === "torrent" || t.link_type === "https") {
-        peerInput = '<div class="peer-row"><input class="peer-input" type="text" placeholder="Add peer IP:port"><button class="btn btn-sm btn-secondary peer-btn" onclick="addPeer('' + t.id + '')">Connect</button></div>';
+        peerInput = '<div class="peer-row"><input class="peer-input" type="text" placeholder="Add peer IP:port"><button class="btn btn-sm btn-secondary peer-btn" onclick="addPeer(\'" + t.id + "\')">Connect</button></div>';
     }
     if (t.status === "downloading") {
       actionButtons += '<button class="btn btn-sm btn-secondary btn-pause" title="Pause"><svg width="14" height="14" viewBox="0 0 24 24" fill="currentColor"><rect x="6" y="4" width="4" height="16"/><rect x="14" y="4" width="4" height="16"/></svg></button>';
